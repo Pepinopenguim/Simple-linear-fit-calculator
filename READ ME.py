68eea8a4164e9d518895108d67e0cba3881410cc
Pepinopenguim <144067863+Pepinopenguim@users.noreply.github.com>
2023-11-14 21:23:13 -0300

    READ ME.txt

---
+++ READ ME.py
@@ -0,0 +1,6 @@
+This is a VERY simple linear fit calculator I've created to help (procrastinate) my studies of Calculus 4 (Numeric Calculus)
+
+I've not used ChatGPT for help on this one... OK only to add commentaries because I was lazy
+I plan to improve this if I feel like it, it's not hard to make a similar code for a parabola or etc
+
+- Pepinopenguim
